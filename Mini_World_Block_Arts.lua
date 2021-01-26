1cf496ba57b85c2295773168096839e302f5561b
furasesa <angga.furasesa@gmail.com>
2021-01-26 16:43:20 +0700

    Feature: Enchantment

--- Mini_World_Block_Arts.lua
+++ Mini_World_Block_Arts.lua
@@ -1,28 +1,31 @@
---apk_version = 0.50.0
---gg_version = 101
---script_version = 0.0.1
+-- apk_version = 0.50.0; 0.51.0
+-- gg_version = 101
+-- script_version = 0.0.1
+-- AUTHOR = Furasesa
 
---AUTHOR = Furasesa
-
--- local key.xor = 945804460 --Define your own
+-- DEFAULT KEY
 local def_XOR_KEY = 945804460
-local def_key_addr = 506115623932
+local def_key_addr = 0x751A25AD7C --for development only
+-- local def_enchant_ln = 31
 
-local debug = true
+local debug = true 
 local resume_state = {['func']= 0, ['bookmark']=0}
 
 local key = {['xor'] = def_XOR_KEY, ['address']=def_key_addr}
-local item_address = {} -- t{[1]=t{}, [2]=t{}, etc}
-item_address[8] = {}
+local item_address = {}
+item_address[8] = {} -- Use for key address
+
 -- local msg_prepare_1 = 'Catch Key Address from your item which is stackable'
 local msg_prepare_1 = 'Set Item 8 to stackable item. and select how many it is.'
 local msg_prepare_2 = 'Now change the value by increasing or decreasing in 5s'
 
 local str_info
 
-local item_type = ({'Tools', 'Equipment', 'Stackable'})
+local item_type = {[0]='Back','Tools', 'Equipment', 'Stackable'}
+local item_list = {[0]='Back','item 1','item 2','item 3','item 4','item 5','item 6','item 7','item 8'}
+local item_mod_type = {[0]='Back','Upgrade', 'Durability', 'Enchant'}
 
-local Edit_Table = {'Soil', 'Iron Ore', 'Leather Helmet', 'Wooden Pickaxe'}
+-- local Edit_Table = {'Soil', 'Iron Ore', 'Leather Helmet', 'Wooden Pickaxe'}
 
 local soil_edit = {
     [0] = "Soil",
@@ -63,62 +66,40 @@ local iron_ore_edit = {
     [16] = "Kyanite Block",
 }
 
-local leather_helmet_edit = {
-    [-3] = "Red Fortune Bag",
-    [-1] = "Purple Fortune Bag",
-    [0] = "Leather Helmet",
-    [1] = "Leather Armor",
-    [2] = "Leather Greave",
-    [3] = "Leather Boot",
-    [4] = "Leather Cloak",
-    [5] = "Gold Embroidered Cloak",
-    [6] = "Diamond Cloak",
-    [7] = "Kyanite Cloak",
-    [8] = "Iron Encusted Cloak",
-    [9] = "Lava Cloak",
+local enchant_table = {
+    [5] = {['name'] = 'Launch Strike', ['lvl']=5},
+    [6] = {['name'] = "Sharp", ['lvl']=5},
+    [7] = {['name'] = "Human Hunter", ['lvl']=5},
+    [8] = {['name'] = "Animal Hunter", ['lvl']=5},
+    [9] = {['name'] = "Demon Hunter", ['lvl']=5},
+    [10] = {['name'] = "Ignite", ['lvl']=5},
+    [11] = {['name'] = "KnockBack", ['lvl']=5},
+    [12] = {['name'] = "Lucky Hunting", ['lvl']=5},
+    [13] = {['name'] = "Powerful Shoot", ['lvl']=5},
+    [14] = {['name'] = "Unlimited Shoot", ['lvl']=5},
+    [15] = {['name'] = "Durability", ['lvl']=5},
+    [16] = {['name'] = "Melee Protection", ['lvl']=5},
+    [17] = {['name'] = "Long-Range Protection", ['lvl']=5},
+    [18] = {['name'] = "Blast Protection", ['lvl']=5},
+    [19] = {['name'] = "Burning Protection", ['lvl']=5},
+    [20] = {['name'] = "Poison Protection", ['lvl']=5},
+    [21] = {['name'] = "Chaos Protection", ['lvl']=5},
+    [22] = {['name'] = "Counter Strike", ['lvl']=5},
+    [23] = {['name'] = "KnockBack Resistance", ['lvl']=5},
+    [24] = {['name'] = "Precise Collection", ['lvl']=1},
+    [25] = {['name'] = "Speed", ['lvl']=5},
+    [26] = {['name'] = "Lucky Digging", ['lvl']=5},
+    [27] = {['name'] = "Blast Shoot", ['lvl']=1},
+    [28] = {['name'] = "Dragon Slow Falling", ['lvl']=1},
+    [29] = {['name'] = "Savage Hunter", ['lvl']=5},
+    [30] = {['name'] = "Speedy Falling", ['lvl']=5},
+    [31] = {['name'] = "Spider Climbing", ['lvl']=5},
 }
 
-local wooden_pickaxe_edit ={ [2]='Gold Pickaxe'  }
-
-local stone_spear_edit = {[1]='Iron Sword'}
-
-local enchant_tool_table = {
-    [15] = "Durability",
-    [25] = "Speed",
-    [24] = "Precise Collection",
-    [26] = "Lucky Digging",
-}
-
-local enchant_weapon_table = {
-    [5] = "Launch Strike (V Knockback)",
-    [6] = "Sharp (Melee Dmg)",
-    [7] = "Human Hunter",
-    [8] = "Animal Hunter",
-    [9] = "Demon Hunter",
-    [10] = "Ignite (Burn)",
-    [11] = "KnockBack (H Knockback)",
-    [12] = "Lucky Hunting",
-    [13] = "Powerful Shoot (Bow)",
-    [14] = "Unlimited Shoot (Bow)",
-    [27] = "Blast Shoot (Bow) [1]",
-    [15] = "Durability"
-}
-
-
-local enchant_amor_table = {
-    [15] = "Durability",
-    [16] = "Melee Protection",
-    [17] = "Long-Range Protection",
-    [18] = "Blast Protection",
-    [19] = "Burning Protection",
-    [20] = "Poison Protection",
-    [21] = "Chaos Protection",
-    [22] = "CounterStrike (Reflect Dmg)",
-    [23] = "KnockBack Resistance",
-    [24] = "Dragon Slow Falling (Cloak)",
-    [30] = "Speedy Falling (Greave)",
-    [31] = "Spider Climb (Boot)",
-}
+local enc_tool_table = {15,24,25,26,6,7,8,9,29}
+local enc_weapon_table = {5,11,10,6,13,14,27,7,8,9,12,15}
+local enc_armor_table = {15,16,17,18,19,20,21,22,23,28,30,31 }
+local enc_all = {5,11,10,6,13,14,27,7,8,9,12,15,16,17,18,19,20,21,22,23,28,30,31,24,25,26,29}
 
 function Key_Address_Validator()
     if debug then print('-----Key_Address_Validator-----') end
@@ -185,10 +166,10 @@ function Key_Address_Finder()
         if debug then print('result count :', cnt) end
         if cnt == 0 then Key_Address_Finder() end
         gg.toast(msg_prepare_2)
-        gg.sleep(5000)
+        
         while cnt > 1 do
             if debug then print('refine number ~=', search_str) end
-            gg.sleep(200)
+            gg.sleep(5000)
             gg.refineNumber(search_str, gg.TYPE_DWORD, false, gg.SIGN_NOT_EQUAL)
             cnt = gg.getResultsCount()
             res = gg.getResults(100)
@@ -227,53 +208,134 @@ end
 --resume num = 3
 function Item_Manipulator()
     resume_state.func = 3
-    local item_ch = gg.choice({'item 1','item 2','item 3','item 4','item 5','item 6','item 7','item 8','Back'})
+    local gen_enchant_table = function (v_table)
+        local res = {}
+        res.id = v_table
+        res.name = {}
+        res.lvl = {}
+        for _, v in ipairs(v_table) do
+            res.name[#res.name+1] = enchant_table[v].name
+            res.lvl[#res.lvl+1] = enchant_table[v].lvl
+        end
+        return res
+    end
+    local gen_lvl_prompt = function (selected_enchant, gen_tbl)
+        local res = {}
+        res.name = {}
+        res.default = {}
+        res.type = {}
+        for i,v in pairs(selected_enchant) do
+            if v then
+                res.name[#res.name+1] = gen_tbl.name[i]..'[1;'..gen_tbl.lvl[i]..']'
+                res.default[#res.default+1] = gen_tbl.lvl[i]
+                res.type[#res.type+1] = 'number'
+            end
+        end
+        return res
+    end
+
+    local gen_enchant_value = function (selected_enchant, lvl_value, gen_table_reference)
+        local res = {}
+        local seltmp ={}
+        for i, v in pairs(selected_enchant) do
+            if v then seltmp[#seltmp+1] = gen_table_reference.id[i] end
+        end
+        for i, v in ipairs(lvl_value) do res[#res+1] = seltmp[i]..'0'..lvl_value[i] end
+        return res
+    end
+
+    ::ITEM_LIST::
+    local item_ch = gg.choice(item_list)
     if item_ch == nil then Hide() end
-    if item_ch == 9 then Main() end
+    if item_ch == 0 then Main() end
+
+    ::ITEM_TYPE::
     local item_tp_ch = gg.choice(item_type) --tools, Equipment, stackable
-    if item_tp_ch == nil then Item_Manipulator() end
-
-    --get address selected
-    -- local temp= {}
-    -- temp[1]={}
-    -- temp[1].address = item_address[item_ch].address
-    -- temp[1].flags = 4
-
-    if item_tp_ch == 1 then
-        --tools
-        local tool_ch = gg.choice({'Back','Upgrade', 'Durability', 'Enchant'})
-        if tool_ch == 1 then Item_Manipulator()
-        elseif tool_ch == 2 then --Upgrade
+    if item_tp_ch == nil then Hide() end
+    if item_tp_ch == 0 then goto ITEM_LIST end
+    -- ::ITEM_MOD_TYPE::
+    if item_tp_ch == 1 then --Tools
+        local tool_ch = gg.choice(item_mod_type)
+        if tool_ch == 0 then goto ITEM_TYPE
+        elseif tool_ch == 1 then -- Upgrade
             local up_level = gg.prompt({'Upgrade Level [0;4]'}, {'0'}, {'number'})
             if up_level == nil then Item_Manipulator()
             else
                 Equipment_Upgrade(item_address[item_ch].address, up_level[1])
             end
-        elseif tool_ch == 3 then --Durability
+
+        elseif tool_ch == 2 then -- Durability
             Durability_Editor(item_address[item_ch].address)
-        elseif tool_ch == 4 then
-            gg.alert('comming soon')
+
+        elseif tool_ch == 3 then -- Enchant
+            local _ch = gg.choice({'Tool', 'All List'})
+            local ch_wa = {}
+            if _ch == 1 then ch_wa = enc_tool_table
+            elseif _ch == 2 then ch_wa = enc_all
+            else Hide()
+            end
+
+            local gen_tbl = gen_enchant_table(ch_wa)
+            if debug then print('gen tbl', gen_tbl) end
+
+            local sel_enchant = gg.multiChoice(gen_tbl.name)
+            if debug then print('selected enchant', sel_enchant) end
+
+            -- gen level
+            local lvl_prmp = gen_lvl_prompt(sel_enchant, gen_tbl)
+            if debug then print('level prompt :', lvl_prmp) end
+
+            local lvl_val = gg.prompt(lvl_prmp.name, lvl_prmp.default, lvl_prmp.type)
+            if debug then print('promp result', lvl_val) end
+
+            local enc_val = gen_enchant_value(sel_enchant, lvl_val, gen_tbl)
+            if debug then print('enchant value :', enc_val)end
+
+            Enchantment(item_address[item_ch].address, enc_val)
+            
             Item_Manipulator()
         else Hide()
         end
-    elseif item_tp_ch == 2 then
-        --equipment
-        local eq_ch = gg.choice({'Back','Upgrade', 'Durability', 'Enchant'})
-        if eq_ch == 1 then Item_Manipulator()
-        elseif eq_ch == 2 then --Upgrade
+    elseif item_tp_ch == 2 then --Equipment
+        local eq_ch = gg.choice(item_mod_type)
+        if eq_ch == 0 then goto ITEM_TYPE
+        elseif eq_ch == 1 then -- Upgrade
             local up_level = gg.prompt({'Upgrade Level [0;3]'}, {'0'}, {'number'})
             if up_level == nil then Item_Manipulator()
             else
                 Equipment_Upgrade(item_address[item_ch].address, up_level[1])
             end
-        elseif eq_ch == 3 then --Durability
+        elseif eq_ch == 2 then -- Durability
             Durability_Editor(item_address[item_ch].address)
-        elseif eq_ch == 4 then
-            gg.alert('comming soon')
+        elseif eq_ch == 3 then -- Enchant
+            local _ch = gg.choice({'Weapon', 'Armor', 'All List'})
+            local ch_wa = {}
+            if _ch == 1 then ch_wa = enc_weapon_table
+            elseif _ch == 2 then ch_wa = enc_armor_table
+            elseif _ch == 3 then ch_wa = enc_all
+            else Hide()
+            end
+            local gen_tbl = gen_enchant_table(ch_wa)
+            if debug then print('gen tbl', gen_tbl) end
+
+            local sel_enchant = gg.multiChoice(gen_tbl.name)
+            if debug then print('selected enchant', sel_enchant) end
+
+            -- gen level
+            local lvl_prmp = gen_lvl_prompt(sel_enchant, gen_tbl)
+            if debug then print('level prompt :', lvl_prmp) end
+
+            local lvl_val = gg.prompt(lvl_prmp.name, lvl_prmp.default, lvl_prmp.type)
+            if debug then print('promp result', lvl_val) end
+
+            local enc_val = gen_enchant_value(sel_enchant, lvl_val, gen_tbl)
+            if debug then print('enchant value :', enc_val)end
+
+            Enchantment(item_address[item_ch].address, enc_val)
             Item_Manipulator()
         else Hide()
         end
-    elseif item_tp_ch == 3 then
+    elseif item_tp_ch == 3 then --Stackable
         --stackable
         local stack_size = gg.prompt({'Stack Size [1;64]'},{'1'},{'number'})
         if stack_size == nil then Item_Manipulator()
@@ -413,6 +475,32 @@ function Equipment_Upgrade(address, val)
     
 end
 
+function Enchantment(address, tbl_enc)
+    if debug then print('-----Enchantment-----') end
+    local offset_enc_act = 0x8
+    local offset_ench = 0x4
+    local temp = {}
+
+    for i in ipairs(tbl_enc) do
+        temp[i] = {}
+        temp[i].address = address + offset_enc_act + i*offset_ench
+        temp[i].flags = 4
+        temp[i].value = tbl_enc[i]
+    end
+
+    local num_activated = #temp
+    if debug then print('num enchant:', num_activated) end
+
+    temp[num_activated+1] = {}
+    temp[num_activated+1].address = address + offset_enc_act
+    temp[num_activated+1].flags = 4
+    temp[num_activated+1].value = num_activated
+
+    print('template', temp)
+    gg.setValues(temp)
+    gg.alert('Enchant Done')
+end
+
 function Home_Info()
     Key_Address_Validator()
     str_info = string.format("key.xor :%d\nKEY Address :%s", key.xor, key.address) 
@@ -464,36 +552,6 @@ function Instruction_Info()
     local info = '1. Put Soil/Iron Ore/etc at Item #8\n2. Cast Previous Item to Another'
     gg.alert(info)
 end
-
--- function Find_New()
---     if debug then print('-----Find New Sprite Change----') end
---     local ch = gg.choice({'Ready', 'Not Yet'})
---     if debug then print('choice', ch) end
-    
---     local temp = {}
---     temp[1] = {}
---     temp[1].address = item_address[8].address+0x24
---     temp[1].flags = 4
---     temp = gg.getValues(temp)
-    
---     if ch == 1 then
---         -- local cast_to = gg.choice(soil_edit)
---         -- if debug then print ('cast to : ', cast_to)end
---         for i=0, 15 do
---             gg.toast(string.format('number :%d', i))
---             Sprite_Changer(i)
---             gg.sleep(10000)
---         end
-        
---     elseif ch == 2 then Find_New()
---     else Hide()
---     end
-
---     gg.setValues(temp)
--- end
-
-
-
 
 
 
